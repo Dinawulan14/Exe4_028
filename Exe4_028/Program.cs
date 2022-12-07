5fe641e35c0bec1c1c4e567d493da6c7dd23c6ff
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-12-07 15:43:32 +0700

    Membuat class program

--- Exe4_028/Program.cs
+++ Exe4_028/Program.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace Exe4_028
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
-        {
-        }
-    } 
-}
+        //dimana 48 merupakan hasil dari 7+28+1+2x10-10
+        private int[] dina = new int[46];
+        int n;
+        private readonly object arr;
+        private int i;
+    }
+
+    }
