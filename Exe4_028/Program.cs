f260a65a51cd5c641de6d42b36689e51485f0cb4
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-12-07 15:43:55 +0700

    Menambahkan private int

--- Exe4_028/Program.cs
+++ Exe4_028/Program.cs
@@ -9,7 +9,7 @@ namespace Exe4_028
     class Program
     {
         //dimana 48 merupakan hasil dari 7+28+1+2x10-10
-        private int[] dina = new int[46];
+        private int[] dina = new int[46]; 
         int n;
         private readonly object arr;
         private int i;
